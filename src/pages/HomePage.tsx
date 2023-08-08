d4e7822626484340f6d29fa1502d473f37d7792d
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-08-08 17:35:29 +0200

    Created homePage logic, added axios packages, eslint configuration

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,50 @@
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+interface pizzaName {
+  name: string;
+}
 const HomePage = () => {
+  const [pizzaData, setPizzaData] = useState<pizzaName[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPizzaName = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/pizza/get/all`,
+        );
+        const data = response.data.pizzas;
+
+        if (data) {
+          setPizzaData(data);
+        } else {
+          setError('Something went wrong. Please, try again later.');
+        }
+      } catch (error: any) {
+        setError(
+          error.response?.data.message ||
+            'Something went wrong. Please, try again later.',
+        );
+      }
+    };
+    fetchPizzaName();
+  }, []);
+
   return (
-    <div>
-      HomePage
-      <Link to={`/pizza/${5}`}>Pizza 5</Link>
-      <Link to={`/pizza/${6}`}>Pizza 6</Link>
-    </div>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {pizzaData &&
+          !error &&
+          pizzaData.map((pizza) => (
+            <Link to={`/pizza/${pizza.name}`} key={pizza.name}>
+              {pizza.name}
+            </Link>
+          ))}
+      </div>
+      {error ? <p>{error}</p> : null}
+    </>
   );
 };
 export default HomePage;
