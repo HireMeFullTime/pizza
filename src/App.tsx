20f790ab2b771a40772f75f20999568221f9d000
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-08-11 01:01:01 +0200

    Added Error to not found route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import { Suspense, lazy } from 'react';
 
+import Error from './components/Error';
 import Layout from './components/layout/Layout';
 import Spinner from './components/Spinner';
 
@@ -28,6 +29,7 @@ function App() {
             />
             <Route path="/actions" element={<Actions />} />
             <Route path="/actions/:actionName" element={<ActionDetails />} />
+            <Route path="*" element={<Error content="Page not found" />} />
           </Route>
         </Routes>
       </Suspense>
